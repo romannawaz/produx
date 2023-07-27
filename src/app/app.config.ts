beb409197cb271199bddbd5a04584e1368ceec44
romannawaz <romannawaz01@gmail.com>
2023-07-27 19:03:02 +0300

    feat: Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { rootStoreDevConfig } from '@app/store/root/root-store.config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(HttpClientModule), rootStoreDevConfig],
+  providers: [importProvidersFrom(HttpClientModule), rootStoreDevConfig, provideAnimations()],
 };
