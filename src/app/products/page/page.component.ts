21440afd0190c24f26e52fdf9dd539aa6158466d
romannawaz <romannawaz01@gmail.com>
2023-07-28 12:22:32 +0300

    feat(product.page): product cards

--- src/app/products/page/page.component.ts
+++ src/app/products/page/page.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductStoreModule } from '../state/product-store.module';
+import { ProductFacade } from '../state/product.facade';
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent, ProductStoreModule],
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
 })
-export class PageComponent {}
+export class PageComponent {
+  constructor(public readonly productFacade: ProductFacade) {}
+}
