c340680fce05b19c70b97dc1063e481893638a26
romannawaz <romannawaz01@gmail.com>
2023-07-28 12:20:57 +0300

    feat(product-card): simple ui

--- src/app/products/page/components/product-card/product-card.component.ts
+++ src/app/products/page/components/product-card/product-card.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+
+import { Product } from '@app/products/common';
+import { ProductStoreModule } from '@app/products/state/product-store.module';
+import { ProductFacade } from '@app/products/state/product.facade';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule, ProductStoreModule],
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
+  faEye = faEye;
+  faCalendar = faCalendar;
+  faCircleXmark = faCircleXmark;
 
+  @Input({ required: true })
+  product!: Product;
+
+  constructor(private readonly productsFacade: ProductFacade) {}
+
+  remove(): void {
+    console.log(this.product.id);
+    this.productsFacade.remove(this.product.id);
+  }
 }
