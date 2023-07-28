8b73eacc94062e29342577cc12d3775e62e1f648
romannawaz <romannawaz01@gmail.com>
2023-07-28 21:35:38 +0300

    feat(app): header with create product button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,40 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
+import { ProductFacade } from '@app/products/state/product.facade';
+import { ProductStoreModule } from '@app/products/state/product-store.module';
+import { ProductDialogComponent } from '@app/products/page/dialogs/product-dialog/product-dialog.component';
+
+const Material = [MatButtonModule, MatDialogModule];
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ProductStoreModule,
+    ...Material,
+    FontAwesomeModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  faHeart = faHeart;
+
+  constructor(
+    public readonly productFacade: ProductFacade,
+    private readonly dialog: MatDialog
+  ) {}
+
+  createDialog(): void {
+    this.dialog.open(ProductDialogComponent, { data: { type: 'create' } });
+  }
+}
