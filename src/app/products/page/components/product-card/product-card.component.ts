4fe674cdff3cf4c953cb7d5ae77c3d349f57b5e2
romannawaz <romannawaz01@gmail.com>
2023-07-28 21:26:09 +0300

    feat(product-card): updated styles and added edit button

--- src/app/products/page/components/product-card/product-card.component.ts
+++ src/app/products/page/components/product-card/product-card.component.ts
@@ -1,19 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye } from '@fortawesome/free-regular-svg-icons';
 import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { faEdit } from '@fortawesome/free-regular-svg-icons';
 
 import { Product } from '@app/products/common';
 import { ProductStoreModule } from '@app/products/state/product-store.module';
 import { ProductFacade } from '@app/products/state/product.facade';
+import { ProductDialogComponent } from '@app/products/page/dialogs/product-dialog/product-dialog.component';
+
+const Material = [MatDialogModule];
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule, ProductStoreModule],
+  imports: [CommonModule, ...Material, FontAwesomeModule, ProductStoreModule],
+  providers: [MatDialog],
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
@@ -21,11 +27,24 @@ export class ProductCardComponent {
   faEye = faEye;
   faCalendar = faCalendar;
   faCircleXmark = faCircleXmark;
+  faEdit = faEdit;
 
   @Input({ required: true })
   product!: Product;
 
-  constructor(private readonly productsFacade: ProductFacade) {}
+  constructor(
+    private readonly productsFacade: ProductFacade,
+    private readonly dialog: MatDialog
+  ) {}
+
+  edit(): void {
+    this.dialog.open(ProductDialogComponent, {
+      data: {
+        type: 'update',
+        product: this.product,
+      },
+    });
+  }
 
   remove(): void {
     console.log(this.product.id);
